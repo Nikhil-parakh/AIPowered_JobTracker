be42a48bd3292eaee67658cfaf790d71cb02dbf5
Nikhil-parakh <nickparakh098@gmail.com>
2026-05-24 20:29:31 +0530

    Initial Commit

---
+++ AIPowered_JobTracker/src/main/java/com/Project/AIPowered_JobTracker/AiPoweredJobTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.Project.AIPowered_JobTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredJobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredJobTrackerApplication.class, args);
+	}
+
+}
